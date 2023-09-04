a99665e6f3ccdbc0fc37a0c137490c72b41a798f
Sid-Montalvo <pizzza.planeta@gmail.com>
2023-09-04 14:20:02 -0600

    Service Worker eliminado

--- src/app.js
+++
@@ -1,8 +0,0 @@
-if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('./service-worker.js')
-        .then(registrado  => console.log('Service Worker registrado con éxito:', registrado))
-        .catch(error => console.log('Error al registrar el Service Worker:', error));
-    } else {
-      console.log('Service Workers no soportados');
-    }
-  
